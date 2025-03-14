153fc6192f8bf15d7ed0adcce14691f6655e5c0a
Tevin Cheatham <tevincheatham@Tevins-MacBook-Pro.local>
2025-03-14 17:00:37 -0400

    production dbURI

--- src/Models/index.js
+++ src/Models/index.js
@@ -1,11 +1,11 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
 const { User } = require("./User/User");
 const { Family } = require("./Family/Family");
 const { Room } = require("./Room/Room");
 
 // MongoDB URI
-const dbURI =
-  "mongodb+srv://tevindcheatham:Bridget1003@cluster0.bpmoe.mongodb.net/";
+const dbURI = process.env.MONGO_URI;
 
 const db = async () => {
   // establish connection to MongoDB
