f9f92638b1c1ec55358da9374ac6ecd0664c0a28
Tevin Cheatham <tevincheatham@Tevins-MacBook-Pro.local>
2025-03-14 18:47:28 -0400

    routing fixes

--- src/View/src/main.jsx
+++ src/View/src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/'>
+    <BrowserRouter>
         <Routes>
           <Route path='/*' element={<App />} />
         </Routes>
