48c88179d650bf40ae6f405a10317a01210de7f6
Tevin Cheatham <tevincheatham@Tevins-MacBook-Pro.local>
2025-03-16 17:11:29 -0400

    fix useEffect on Root component

--- src/View/src/routes/Root.jsx
+++ src/View/src/routes/Root.jsx
@@ -1,13 +1,20 @@
 import { Outlet } from "react-router-dom";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useContext } from "react";
+import { UserContext } from "./userContext/UserContext";
 
 export default function Root() {
   const location = useLocation();
   const navigate = useNavigate();
 
-  // useEffect(() => {
-  //     if (location.pathname === '/') navigate('/register');
-  //   }, [location]);
+  const { user } = useContext(UserContext);
+
+  useEffect(() => {
+      if (location.pathname === '/') {
+        if (!user) navigate('/register');
+        else navigate("/home");
+      }
+    }, [location]);
 
   return <Outlet />;
 }
